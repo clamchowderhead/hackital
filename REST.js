44196e54062b407e02315f37d62c153a98e53823
System Administrator <root@Adnans-MacBook-Air.local>
2017-11-12 13:44:36 -0500

    Adding the GET REST methods

--- REST.js
+++ REST.js
@@ -5,8 +5,34 @@ function REST_ROUTER(router,connection,md5) {
 }
 
 REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
-    router.get("/",function(req,res){
+
+    router.get("/rants/",function(req,res){
+    	var query = "SELECT * FROM ??";
+        var table = ["rant"];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Rants" : rows});
+            }
+        });
     });
+
+
+    router.get("/compliments/",function(req,res){
+    	var query = "SELECT * FROM ??";
+        var table = ["compliments"];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Compliments" : rows});
+            }
+        });
+    });
+
     router.post("/rants",function(req,res){
         var query = "INSERT INTO ??(??, ??) VALUES (NULL,?)";
         var table = ["rant","id","rant",req.body.rant];
@@ -16,6 +42,19 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
                 res.json({"Error" : true, "Message" : "Error executing MySQL query"});
             } else {
                 res.json({"Error" : false, "Message" : "Rant Added !"});
+            }
+        });
+    });
+
+    router.post("/compliments",function(req,res){
+        var query = "INSERT INTO ??(??, ??,??) VALUES (NULL,?,NULL)";
+        var table = ["compliments","id","compliment","rating",req.body.compliments];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Compliment Added !"});
             }
         });
     });
