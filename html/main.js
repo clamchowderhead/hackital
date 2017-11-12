d77d17f9ee9a31706dae18ef4928b3c79ed973ce
jmekavibul <jm9hp@virginia.edu>
2017-11-12 00:01:10 -0500

    sleepo
    sleepo

---
+++ html/main.js
@@ -0,0 +1,9 @@
+function ran_col() { //function name
+    var color = '#'; // hexadecimal starting symbol
+    var letters = ["aec6cf","a4b6dd","d09292","c094cc","c37892","9ecb77","cb779e","a477cb"]; //Set your colors here
+    color += letters[Math.floor(Math.random() * letters.length)];
+    document.body.style.background = color; // Setting the random color on your div element.
+    var list = document.getElementsByClassName("btn btn-rant");
+    list[0].style.background = color;
+    list[1].style.background = color;
+  }
